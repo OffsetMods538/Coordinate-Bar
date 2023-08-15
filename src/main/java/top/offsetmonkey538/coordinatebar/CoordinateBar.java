b2c870caad2fc98a803b995f1ff0df1ff22086f0
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-08-15 17:15:26 +0300

    Figure out the correct way to do a boss bar

--- src/main/java/top/offsetmonkey538/coordinatebar/CoordinateBar.java
+++ src/main/java/top/offsetmonkey538/coordinatebar/CoordinateBar.java
@@ -1,9 +1,13 @@
 package top.offsetmonkey538.coordinatebar;
 
+import java.util.HashMap;
+import java.util.Map;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.entity.boss.BossBar;
 import net.minecraft.entity.boss.BossBarManager;
 import net.minecraft.entity.boss.CommandBossBar;
+import net.minecraft.entity.boss.ServerBossBar;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
@@ -17,7 +21,7 @@ public class CoordinateBar implements ModInitializer {
 	public static final String MOD_ID = "coordinate-bar";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	public static final Identifier POSITION_BAR = id("position_bar");
+	public static final Map<ServerPlayerEntity, ServerBossBar> POSITION_BAR_MAP = new HashMap<>();
 
 	@Override
 	public void onInitialize() {
@@ -26,37 +30,23 @@ public class CoordinateBar implements ModInitializer {
 		CommandRegistrationCallback.EVENT.register(((dispatcher, registryAccess, environment) -> CoordinateBarCommand.register(dispatcher)));
 	}
 
-	public static Identifier positionBarId(ServerPlayerEntity player) {
-		return POSITION_BAR.withSuffixedPath("_" + player.getUuidAsString());
-	}
-
 	public static void addPositionBar(ServerPlayerEntity player) {
-		final MinecraftServer server = player.getServer();
-		if (server == null) return;
-		final BossBarManager manager = server.getBossBarManager();
-
-		CommandBossBar positionBar = manager.add(positionBarId(player), Text.empty());
+		ServerBossBar positionBar = new ServerBossBar(Text.empty(), BossBar.Color.WHITE, BossBar.Style.PROGRESS);
 		positionBar.addPlayer(player);
 		positionBar.setVisible(true);
+
+		POSITION_BAR_MAP.put(player, positionBar);
 	}
 
 	public static void removePositionBar(ServerPlayerEntity player) {
-		final MinecraftServer server = player.getServer();
-		if (server == null) return;
-		final BossBarManager manager = server.getBossBarManager();
-
-		manager.remove(manager.get(positionBarId(player)));
+		if (!POSITION_BAR_MAP.containsKey(player)) return;
+		POSITION_BAR_MAP.get(player).clearPlayers();
+		POSITION_BAR_MAP.remove(player);
 	}
 
 	public static void updatePositionBar(ServerPlayerEntity player, BlockPos pos) {
-		final MinecraftServer server = player.getServer();
-		if (server == null) return;
-		final BossBarManager manager = server.getBossBarManager();
-
-		CommandBossBar positionBar = manager.get(positionBarId(player));
-		if (positionBar == null) return;
-
-		positionBar.setName(Text.of(String.format("X: %s Y: %s Z: %s", pos.getX(), pos.getY(), pos.getZ())));
+		if (!POSITION_BAR_MAP.containsKey(player)) return;
+		POSITION_BAR_MAP.get(player).setName(Text.of(String.format("X: %s Y: %s Z: %s", pos.getX(), pos.getY(), pos.getZ())));
 	}
 
 	public static Identifier id(String path) {
